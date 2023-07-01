5d72fb7018d382ead4d603c7485f83a95722c5b0
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-06-30 23:23:08 -0500

    Implemented winRound function and updated index.html structure and class names

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ function getComputerChoice(){
     return Math.floor((Math.random() * 3) + 1);
 }
 
-function playRound(playerSelection, computerSelection){
+function winRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return null;
+    } 
+    else if ((playerSelection === 1 && computerSelection === 3) || (playerSelection === 2 && computerSelection === 1) || (playerSelection === 3 && computerSelection === 2)) {
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+/*
+function playRound{
 
 }
+
+function game(){
+
+}
+
+*/
