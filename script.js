a3a7a315d6a3cd7fff70a560a4daba7e481deb3c
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2023-06-30 22:36:34 -0500

    Created basic ui and 'getComputerChoice' function

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+rock = 1
+paper = 2
+scissors = 3
+*/
+
+function getComputerChoice(){
+    return Math.floor((Math.random() * 3) + 1);
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
